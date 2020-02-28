6f7d5df232549d557b0d8768807d1607686b72f2
Gwydion-Meredydd <310137@gllm.ac.uk>
2020-02-28 16:47:59 +0000

    MainMenu
    MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Animator CameraAnimator;
+    public void Play()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Instructions() 
+    {
+        CameraAnimator.SetBool("Instructions", true);
+    }
+    public void Settings() 
+    {
+        CameraAnimator.SetBool("Settings", true);
+    }
+    public void Credits() 
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Exit() 
+    {
+        CameraAnimator.SetBool("Exit", true);
+    }
+    public void ExitReturn()
+    {
+        CameraAnimator.SetBool("Exit", false);
+    }
+    public void Return() 
+    {
+        CameraAnimator.SetBool("Instructions", false);
+        CameraAnimator.SetBool("Settings", false);
+        CameraAnimator.SetBool("Exit", false);
+    }
+
+}
