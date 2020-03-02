c04d83d1c415a51c3e65de6a4edb3ea63956ee4e
Gwydion Meredydd <49554392+Gwydion-Meredydd@users.noreply.github.com>
2020-03-02 14:26:33 +0000

    cUTSCENE 2

--- Assets/Scripts/QuestsController.cs
+++ Assets/Scripts/QuestsController.cs
@@ -7,13 +7,30 @@ using UnityEngine.UI;
 public class QuestsController : MonoBehaviour
 {
     public GameObject Player;
-    public Animation QuestText;
+    public Animator QuestText;
+    public GameObject[] Border;
+    public GameObject[] VCams;
     public int GrabAmmount;
     public void QuestCompleted()
     {
         Player.SendMessage("QuestCompleted");
-        QuestText.Play;
+        StartCoroutine(QuestTextTiming());
+
     }
+    IEnumerator QuestTextTiming()
+    {
+        QuestText.SetBool("Complete", true);
+        yield return new WaitForSeconds(2.2f);
+        QuestText.SetBool("Complete",false);
+        Border[0].SetActive(false);
+        Border[1].SetActive(true);
+        VCams[0].SetActive(false);
+        VCams[1].SetActive(true);
+        yield return new WaitForSeconds(1);
+        VCams[1].SetActive(false);
+        Border[1].SetActive(false);
+    }
+
 
     public void Grab()
     {
