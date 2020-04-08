203d4aa5247425949d9412495516fd60bdf0640c
Gwydion-Meredydd <310137@gllm.ac.uk>
2020-04-08 20:29:27 +0100

    started stuff got depressed

---
+++ Assets/MerchantManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MerchantManager : MonoBehaviour
+{
+   public void HealthPotion() 
+    {
+
+    }
+    public void ShieldPotion() 
+    {
+
+    }
+    public void ManaPotion() 
+    {
+
+    }
+    public void JumpPotion() 
+    {
+
+    }
+    public void SpeedPotion() 
+    {
+
+    }
+    public void StrengthPotion() 
+    {
+
+    }
+}
