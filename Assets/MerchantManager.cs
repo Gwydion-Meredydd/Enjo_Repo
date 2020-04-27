85f3b9aca9681653f36f743075fd0857b63c7014
Gwydion-Meredydd <310137@gllm.ac.uk>
2020-04-27 01:42:46 +0100

    Credits

--- Assets/MerchantManager.cs
+++ Assets/MerchantManager.cs
@@ -6,36 +6,62 @@ public class MerchantManager : MonoBehaviour
 {
     public int Coin_Ammount;
     public GameObject QuestManager;
-   public void HealthPotion() 
+    public PotionsClass PotionAmmountSave;
+    public void HealthPotion() 
     {
         QuestManager.SendMessage("GetCoinAmmount");
         if (Coin_Ammount <= 60)
         {
-
+            PotionAmmountSave.HealthPotion = PotionAmmountSave.HealthPotion + 1;
         }
     }
     public void ShieldPotion() 
     {
-
+        QuestManager.SendMessage("GetCoinAmmount");
+        if (Coin_Ammount <= 60)
+        {
+            PotionAmmountSave.ShieldPotion = PotionAmmountSave.ShieldPotion + 1; 
+        }
     }
-    public void ManaPotion() 
+    public void ManaPotion()
     {
-
+        QuestManager.SendMessage("GetCoinAmmount");
+        if (Coin_Ammount <= 120)
+        {
+            PotionAmmountSave.ManaPotion = PotionAmmountSave.ManaPotion + 1;
+        }
     }
-    public void JumpPotion() 
+    public void JumpPotion()
     {
-
+        QuestManager.SendMessage("GetCoinAmmount");
+        if (Coin_Ammount <= 60)
+        {
+            PotionAmmountSave.JumpPotion = PotionAmmountSave.JumpPotion + 1;
+        }
     }
-    public void SpeedPotion() 
+    public void SpeedPotion()
     {
-
+        QuestManager.SendMessage("GetCoinAmmount");
+        if (Coin_Ammount <= 60)
+        {
+            PotionAmmountSave.SpeeedPotion = PotionAmmountSave.SpeeedPotion + 1;
+        }
     }
-    public void StrengthPotion() 
+    public void StrengthPotion()
     {
-
+        QuestManager.SendMessage("GetCoinAmmount");
+        if (Coin_Ammount <= 60)
+        {
+            PotionAmmountSave.StrengthPotion = PotionAmmountSave.StrengthPotion + 1;
+        }
     }
     public void CoinReciver(int CoinAmmount) 
     {
         Coin_Ammount = CoinAmmount;
+    }
+    public void MechantExit() 
+    {
+        Debug.Log("Off");
+        gameObject.SetActive(false);
     }
 }
